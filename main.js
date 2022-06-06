d70329f8ba1037d22ebfaf15052fc2fddbf9e0dc
Scottie <scottie@cccareers.org>
2022-06-06 15:50:13 -0700

    SD Top Spots

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
-// write your code here
+$.getJSON("data.json", function(spots) {
+    let spotsData = ""
+   $.each(spots, function(key, value) {
+       spotsData += "<tr>"
+       spotsData += "<td>"+value.name+"</td>"
+       spotsData += "<td>"+value.description+"</td>"
+       spotsData += `<td> <a href = https://www.google.com/maps?q=${value.location[0]},${value.location[1]}>Map</a> </td>`
+       spotsData += "</tr>"
+   })
+   $("table").append(spotsData)
+})
 
 });
+
+
+
